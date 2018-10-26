0d464885a97acdc793c34dc282c266f16d02b1e1
1hk_star <1hk_star@naver.com>
2018-10-26 20:51:19 +0900

    loadfragment

--- app/src/main/java/com/example/a1hk_s/buttom_bar/MainActivity.java
+++ app/src/main/java/com/example/a1hk_s/buttom_bar/MainActivity.java
@@ -10,61 +10,56 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
-    private TextView mTextMessage;
-    private android.support.v4.app.Fragment frag1_home;
-    private android.support.v4.app.Fragment frag2_wargame;
-    private android.support.v4.app.Fragment frag3_community;
-    private android.support.v4.app.Fragment frag4_mypage;
-
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-            //FragmentManager fm = getFragmentManager();
-            android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
-            //FragmentTransaction fragmentTransaction = fm.beginTransaction();
-            android.support.v4.app.FragmentTransaction fragmentTransaction = fm.beginTransaction();
-
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    fragmentTransaction.replace(R.id.frame_layout, frag1_home);
-                    break;
-                case R.id.navigation_wargame:
-                    fragmentTransaction.replace(R.id.frame_layout, frag2_wargame);
-                    break;
-                case R.id.navigation_community:
-                    fragmentTransaction.replace(R.id.frame_layout, frag3_community);
-                    break;
-                case R.id.navigation_mypage:
-                    fragmentTransaction.replace(R.id.frame_layout, frag4_mypage);
-                    break;
-            }
-            fragmentTransaction.commit();
-            return true;
-        }
-
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setOnNavigationItemSelectedListener(this);
 
-        frag1_home = new frag1_home();
-        frag2_wargame = new frag2_wargame();
-        frag3_community = new frag3_community();
-        frag4_mypage = new frag4_mypage();
+        loadFragment(new frag1_home());
 
-        getSupportFragmentManager().beginTransaction().add(R.id.frame_layout,frag1_home).commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        android.support.v4.app.Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new frag1_home();
+                break;
+
+            case R.id.navigation_wargame:
+                fragment = new frag2_wargame();
+                break;
+
+            case R.id.navigation_community:
+                fragment = new frag3_community();
+                break;
+
+            case R.id.navigation_mypage:
+                fragment = new frag4_mypage();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(android.support.v4.app.Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frame_layout, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 
 }
